91df201659541246842e25f2a2757d16f818c6d5
Philip Howard <phil@gadgetoid.com>
2013-03-28 01:10:03 +0000

    More example goodness

--- examples/quick2wire-io.py
+++ examples/quick2wire-io.py
@@ -1,3 +1,4 @@
+# Turns on each pin of an mcp23017 on address 0x20 ( quick2wire IO expander )
 import wiringpi2
 
 pin_base = 65
@@ -10,4 +11,5 @@ wiringpi2.mcp23017Setup(pin_base,i2c_addr)
 for pin in pins:
 	wiringpi2.pinMode(pin,1)
 	wiringpi2.digitalWrite(pin,1)
-
+	wiringpi2.delay(1000)
+	wiringpi2.digitalWrite(pin,0)

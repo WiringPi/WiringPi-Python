95c6d56b1dd1c7b43a5a083fbf10088d0a422bb0
Phil Howard <phil@gadgetoid.com>
2016-02-29 12:07:24 +0000

    Updated to pull headers from wiringpi.i

--- generate-bindings.py
+++ generate-bindings.py
@@ -1,44 +1,14 @@
-HEADERS = [
-"WiringPi/wiringPi/wiringPi.h",
-"WiringPi/wiringPi/wiringPiI2C.h",
-"WiringPi/wiringPi/wiringPiSPI.h",
-"WiringPi/wiringPi/wiringSerial.h",
-"WiringPi/wiringPi/wiringShift.h",
-"WiringPi/wiringPi/wpiExtensions.h",
+HEADERS = []
 
-"WiringPi/wiringPi/drcSerial.h",
-"WiringPi/wiringPi/max31855.h",
-"WiringPi/wiringPi/max5322.h",
-"WiringPi/wiringPi/mcp23008.h",
-"WiringPi/wiringPi/mcp23016.h",
-"WiringPi/wiringPi/mcp23016reg.h",
-"WiringPi/wiringPi/mcp23017.h",
-"WiringPi/wiringPi/mcp23s08.h",
-"WiringPi/wiringPi/mcp23s17.h",
-"WiringPi/wiringPi/mcp23x0817.h",
-"WiringPi/wiringPi/mcp23x08.h",
-"WiringPi/wiringPi/mcp3002.h",
-"WiringPi/wiringPi/mcp3004.h",
-"WiringPi/wiringPi/mcp3422.h",
-"WiringPi/wiringPi/mcp4802.h",
-"WiringPi/wiringPi/pcf8574.h",
-"WiringPi/wiringPi/pcf8591.h",
-"WiringPi/wiringPi/sn3218.h",
-"WiringPi/wiringPi/softPwm.h",
-"WiringPi/wiringPi/softServo.h",
-"WiringPi/wiringPi/softTone.h",
-"WiringPi/wiringPi/sr595.h",
+src = open("wiringpi.i").read().split('\n')
 
-"WiringPi/devLib/ds1302.h",
-"WiringPi/devLib/font.h",
-"WiringPi/devLib/gertboard.h",
-"WiringPi/devLib/lcd128x64.h",
-"WiringPi/devLib/lcd.h",
-"WiringPi/devLib/maxdetect.h",
-"WiringPi/devLib/piFace.h",
-"WiringPi/devLib/piGlow.h",
-"WiringPi/devLib/piNes.h"
-]
+for line in src:
+    line = line.strip()
+    if line.startswith('#include') and line.endswith('.h"'):
+        HEADERS.append(line.replace('#include','').replace('"','').strip())
+
+#print(HEADERS)
+
 def is_c_decl(line):
     for fn in ['wiringPiISR','wiringPiSetupPiFace','wiringPiSetupPiFaceForGpioProg']:
         if fn in line:

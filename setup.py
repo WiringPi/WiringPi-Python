93a6e40cd79e05e29dd1a203ee2b8d097fa07fd3
Mark Liffiton <liffiton@gmail.com>
2021-02-19 23:57:21 -0600

    Bring in latest WiringPi for more hardware compatibility, Arch compile fix.  Bump to 2.60.1.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ _wiringpi = Extension(
 
 setup(
     name = 'wiringpi',
-    version = '2.60.0',
+    version = '2.60.1',
     ext_modules = [ _wiringpi ],
     py_modules = ["wiringpi"],
     install_requires=[],
